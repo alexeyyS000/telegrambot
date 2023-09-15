47406a543f90160aa4d97fb9b750d8bc6a0c0cca
alexey <a556077@icloud.com>
2023-09-15 18:15:16 +0500

    changed parser algorithm

--- app/parser/main.py
+++ app/parser/main.py
@@ -1,24 +1,18 @@
-import websockets
-import asyncio
 import json
 from datetime import datetime
-
+from websockets.sync.client import connect
+from .config import max_rate, min_rate
 
 def parse_better_funding_rate():
-    async def parse_fanding_data():
-        url = "wss://fstream.binance.com/ws/!markPrice@arr"
-        async with websockets.connect(url) as client:
-            data = await client.recv()
-            return data
 
-    loop = asyncio.get_event_loop()
-    data = loop.run_until_complete(parse_fanding_data())
+
+    with connect("wss://fstream.binance.com/ws/!markPrice@arr") as websocket:
+        data = websocket.recv()
+
     data = json.loads(data)
 
     list_better_rate = []
 
-    max_rate = 0.005
-    min_rate = -0.003
     for i in data:
         rate = float(i["r"])
         if rate > max_rate or rate < min_rate:
